1b68f78a5869b334483e69bba5e68e2111e416eb
Siripoom <siripoom.kusonsong@kmutt.mail.ac.th>
2022-09-01 14:41:05 +0700

    code : update

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,15 @@
-const { template } = require('@babel/core')
+const { template } = require("@babel/core");
 
 function calculateBMI(weight, height) {
-  //code here
+  return (bmi = weight / (height * height));
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  if (calculateBMI(weight, height) < 18.5) return "Underweight";
+  else if (
+    calculateBMI(weight, height) >= 18.5 &&
+    calculateBMI(weight, height) <= 24.9
+  )
+    return "Normal weight";
+  else if (calculateBMI(weight, height) > 25) return "Overweight";
 }
-module.exports = getBMIMeaning
+module.exports = getBMIMeaning;
